68ec4828c44fb6c5fe5c51f23375ef336c3a90de
adamrturman <adamrturman@gmail.com>
2021-03-30 15:30:55 -0500

    test for todo is added to list and input is cleared

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
+
+const addAToDo = (input: HTMLElement, value: string, button: HTMLElement) => {
+    fireEvent.change(input, { target: { value: value } });
+    fireEvent.click(button);
+}
 
 test('renders title', () => {
     const { getByText } = render(<App />);
@@ -10,4 +15,16 @@ test('renders title', () => {
 test('renders add button', () => {
     const { getByText } = render(<App />);
     getByText("Click to add");
+});
+
+test('todo is added to list and input is cleared', () => {
+    const { getByText, getByLabelText } = render(<App />);
+    const input = getByLabelText("Add a Todo to the list");
+    const addButton = getByText("Click to add");
+    const inputValue = input.innerHTML;
+
+    addAToDo(input, "get bread", addButton);
+
+    expect(getByText("get bread")).toBeInTheDocument();
+    expect(inputValue).toBe("")
 });
