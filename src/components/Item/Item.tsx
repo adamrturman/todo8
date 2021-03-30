ceb7b093b0afe933502639e7a5ab3f9100b420d4
adamrturman <adamrturman@gmail.com>
2021-03-30 15:44:35 -0500

    delete icon removes todo from DOM

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Todo from "../../interfaces/Todo";
+import ListItem from '@material-ui/core/ListItem';
+import DeleteForever from '@material-ui/icons/DeleteForever';
+
+interface ItemProps {
+    todo: Todo;
+    index: number;
+    deleteTodo: (index: number) => void;
+}
+
+export default function Item (props: ItemProps) {
+
+    const { todo, index, deleteTodo } = props;
+
+    const removeToDo = () => deleteTodo(index);
+
+    return (
+        <>
+            <ListItem key={index}>{todo.text}</ListItem>
+            <DeleteForever onClick={removeToDo}/>
+        </>
+    )
+}
